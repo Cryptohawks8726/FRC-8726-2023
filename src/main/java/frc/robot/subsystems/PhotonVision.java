bf0b995bba0afd197f4506e5b712bb4052d3a655
Pranav Sristy <97794698+Bokelman@users.noreply.github.com>
2023-01-13 16:45:48 -0500

    Added PhotonVision.java

---
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+
+public class PhotonVision {
+    
+    // Change this to match the name of your camera
+    PhotonCamera camera = new PhotonCamera("photonvision");
+
+    // Query the latest result from PhotonVision
+    var result = camera.getLatestResult();
+
+    // Check if the latest result has any targets.
+    boolean hasTargets = result.hasTargets();
+}
