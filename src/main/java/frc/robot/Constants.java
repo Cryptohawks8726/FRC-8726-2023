157f990af8e7f9763243a49cbac66ac759c2a854
source123x <110315300+source123x@users.noreply.github.com>
2023-01-20 17:01:57 -0500

    Updated Constants
    Updated Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //PID Constants
+    public static final double INTAKE_kP = 0.2;
+    public static final double INTAKE_kI = 0.0;
+    public static final double INTAKE_kD = 0.0;
+}
