37aba8f788cbca6cf52ff3ea6d69d36173edc770
source123x <110315300+source123x@users.noreply.github.com>
2023-01-21 12:15:32 -0500

    Implemented PID and Current Limiting
    Implemented SparkMaxPIDController
    Implemented Current Limiting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public final class Constants {
     public static final int DRIVER_XBOX = 0;
     public static final int OPERATOR_XBOX = 1;
+
+    public static final double ARM_kP = 0.005;
+    public static final double ARM_kI = 0;
+    public static final double ARM_kD = 0;
 
     public static final int ARM_MOTOR_SPARKMAX = 51;
 
     public static final double RAISE_ARM_SPEED = 0.5;
     public static final double LOWER_ARM_SPEED = -0.5;
+
+    private static NetworkTableInstance instance;
+    private static NetworkTable table;
+    public static NetworkTableEntry armCurr;
+    public static void setUpNT(){
+        instance = NetworkTableInstance.getDefault();
+        table = instance.getTable("debugValues");
+        armCurr = table.getEntry("armCurr");
+      }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,16 +9,38 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import frc.robot.Constants;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 
 public class ArmSubsystem extends SubsystemBase {
   private CANSparkMax armMotor;
+  private SparkMaxPIDController pid;
+  private RelativeEncoder armEncoder;
 
   public ArmSubsystem() {
     armMotor = new CANSparkMax(Constants.ARM_MOTOR_SPARKMAX, MotorType.kBrushless);
+    armEncoder = armMotor.getEncoder();
+    pid = armMotor.getPIDController(); //The three parameters are the proportional term (position error to 0), the derivative term (velocity error to 0), and the integral term (total accumulated error over time to 0)
+
     armMotor.setIdleMode(IdleMode.kBrake);
+    armMotor.setSmartCurrentLimit(20,20);
+
+    pid.setP(Constants.ARM_kP);
+    pid.setD(Constants.ARM_kD);
+    pid.setI(Constants.ARM_kI);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    Constants.armCurr.setDouble(armMotor.getOutputCurrent()); // gets the velocity instead of current supply (cant find the function)
+  }
+
+  public RelativeEncoder getArmEncoder() {
+    return armEncoder;
   }
 
   public void setArmPower(double speed){
-    armMotor.set(speed);
+    pid.setReference(speed, CANSparkMax.ControlType.kVelocity);
   }
 }
