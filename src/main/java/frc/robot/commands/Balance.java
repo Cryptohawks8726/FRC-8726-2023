92c83326a21c0199c8126551decbdb9b816ab936
emilyy-wang <117683209+Popsa676@users.noreply.github.com>
2023-01-23 15:15:22 -0500

    UpdateBalance

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -4,14 +4,15 @@ package frc.robot.commands;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import com.kauailabs.navx.frc.AHRS;
 
 public class Balance extends CommandBase {
 
     
 
     public Balance() {
-        PIDController pid = new PIDController(Constants.BalanceKp, Constants.BalanceKi, Constants.BalanceKd);
-        
+        PIDController balance = new PIDController(Constants.BalanceKp, Constants.BalanceKi, Constants.BalanceKd);
+        AHRS gyro = new AHRS(SPI.Port.kMXP);
 
 
     }
