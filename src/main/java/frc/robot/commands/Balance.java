c1c1a4dbd6dcba2309a9f4a717b26e9acf484014
emilyy-wang <117683209+Popsa676@users.noreply.github.com>
2023-01-30 15:38:49 -0500

    add initial balance code

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -4,15 +4,32 @@ package frc.robot.commands;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+
 import com.kauailabs.navx.frc.AHRS;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.RobotContainer;
+
 
 public class Balance extends CommandBase {
 
-    
+    public PIDController balance;
+    public AHRS gyro;
 
     public Balance() {
-        PIDController balance = new PIDController(Constants.BalanceKp, Constants.BalanceKi, Constants.BalanceKd);
-        AHRS gyro = new AHRS(SPI.Port.kMXP);
+        balance = new PIDController(Constants.BalanceKp, Constants.BalanceKi, Constants.BalanceKd);
+        gyro = new AHRS(SPI.Port.kMXP);
+        balance.setSetpoint(0);
+
+
+    }
+
+    @Override
+    public void execute() {
+
+        double ang = gyro.getAngle();
+        
+
 
 
     }
