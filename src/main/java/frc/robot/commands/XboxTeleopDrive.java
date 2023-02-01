1797e8d74fca0b166c10f98236b9f5ccfe65e835
Bokelman <110315300+source123x@users.noreply.github.com>
2023-02-01 17:27:06 -0500

    Added Sensitivity for Robot through throttle

--- src/main/java/frc/robot/commands/XboxTeleopDrive.java
+++ src/main/java/frc/robot/commands/XboxTeleopDrive.java
@@ -43,7 +43,7 @@ public class XboxTeleopDrive extends CommandBase{
         // boolean isRobotRelative = controller.leftBumper().getAsBoolean();
 
         boolean isRobotRelative = controller.getTrigger();
-        isRobotRelative=false;
+        isRobotRelative=true;
     
         // Get Controller Values
         // FIXME: xVel and yVel pull the wrong values? Flip them
@@ -54,10 +54,9 @@ public class XboxTeleopDrive extends CommandBase{
         double xVel = (Math.abs(controller.getY()) > 0.1 ? controller.getY() : 0.0); 
         double yVel = (Math.abs(controller.getX()) > 0.1 ? controller.getX() : 0.0);
         double thetaVel = (Math.abs(controller.getZ()) > 0.1 ? controller.getZ() * Constants.Swerve.maxAngularSpeed : 0.0);
-        thetaVel = 0.0;
-
-        xVel = Math.signum(xVel) * Math.pow(xVel,2) * Constants.Swerve.maxSpeed; //square input while preserving sign
-        yVel = Math.signum(yVel) * Math.pow(yVel,2) * Constants.Swerve.maxSpeed;
+        double sensitivity = (controller.getThrottle()*-1)+1.01;
+        xVel = Math.signum(xVel) * Math.pow(xVel,2) * Constants.Swerve.maxSpeed * sensitivity; //square input while preserving sign
+        yVel = Math.signum(yVel) * Math.pow(yVel,2) * Constants.Swerve.maxSpeed * sensitivity;
 
         // maintain heading if there's no rotational input
          if (Math.abs(thetaVel) < 0.1){
