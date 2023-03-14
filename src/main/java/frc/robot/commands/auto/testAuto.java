12dfd71988b9cc92bba6106421a46c62cf1ea037
Pranav Sristy <97794698+Bokelman@users.noreply.github.com>
2023-03-14 17:07:12 -0400

    Update testAuto.java

--- src/main/java/frc/robot/commands/auto/testAuto.java
+++ src/main/java/frc/robot/commands/auto/testAuto.java
@@ -1,7 +1,5 @@
 package frc.robot.commands.auto;
 
-import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -10,6 +8,7 @@ import frc.robot.subsystems.SwerveDrive;
 
 public class testAuto extends CommandBase {
     private SwerveDrive driveTrain;
+
     private boolean flag = true;
     private boolean flag1 = true;
     private boolean flag2 = true;
@@ -44,32 +43,46 @@ public class testAuto extends CommandBase {
 
 
 
-        while(flag1 && !setPosition(1.0, 0.0, 0.0)) {
+        while(flag1 && !setPosition(0.25, 0.0, 0.0)) {
             driveTrain.odometry.update(
                 driveTrain.gyro.getRotation2d(), 
                 driveTrain.getSwerveModulePositions()
             );
         } flag1 = false;
 
-        System.out.println("First Checkpoint");
+        //cone
+        //swing arm up and then outtake
+        //new while block, drive backwards short distance, drop arm
+        //new while block, drive backwards to pass community zone
+
+        //cube
+        //swing arm up
+        //new while loop, drive forwards 16 inch, outtake
+        //new while block, drive backwards short distance, drop arm
+        //new while block, drive backwards to pass community zone ~110 inches
+
+
+
+
+        // System.out.println("First Checkpoint");
         
-        while(flag2 && !setPosition(0.0, 1.0, 0.0)) {
-            driveTrain.odometry.update(
-                driveTrain.gyro.getRotation2d(), 
-                driveTrain.getSwerveModulePositions()
-            );
-        } flag2 = false;
+        // while(flag2 && !setPosition(2.0, 0.0, 0.0)) {
+        //     driveTrain.odometry.update(
+        //         driveTrain.gyro.getRotation2d(), 
+        //         driveTrain.getSwerveModulePositions()
+        //     );
+        // } flag2 = false;
 
-        System.out.println("Second Checkpoint");
+        // System.out.println("Second Checkpoint");
 
-        while(flag3 && !setPosition(0.0, 1.0, 90.0)) {
-            driveTrain.odometry.update(
-                driveTrain.gyro.getRotation2d(), 
-                driveTrain.getSwerveModulePositions()
-            );
-        } flag3 = false;
+        // while(flag3 && !setPosition(3.0, 1.0, 0.0)) {
+        //     driveTrain.odometry.update(
+        //         driveTrain.gyro.getRotation2d(), 
+        //         driveTrain.getSwerveModulePositions()
+        //     );
+        // } flag3 = false;
 
-        System.out.println("Second Checkpoint");
+        // System.out.println("Second Checkpoint");
 
         if(flag) {
             driveTrain.drive(new ChassisSpeeds(0.0, 0.0, 0.0), true);
@@ -86,21 +99,21 @@ public class testAuto extends CommandBase {
         boolean y = false;
         boolean theta = false;
 
-        if (xPos > driveTrain.getPoseEstimate().getX() + 0.05) {
-            xSpeed = 1.0;
+        if (xPos > driveTrain.getPoseEstimate().getX() + 0.03) {
+            xSpeed = 0.4;
             x = false;
-        } else if (xPos < driveTrain.getPoseEstimate().getX() - 0.05){
-            xSpeed = -1.0;
+        } else if (xPos < driveTrain.getPoseEstimate().getX() - 0.03){
+            xSpeed = -0.4;
             x = false;
         } else {
             xSpeed = 0.0;
             x = true;
         }
        
-        if (yPos > driveTrain.getPoseEstimate().getY() + 0.05) {
+        if (yPos > driveTrain.getPoseEstimate().getY() + 0.03) {
             ySpeed = 1.0;
             y = false;
-        } else if (yPos < driveTrain.getPoseEstimate().getY() - 0.05){
+        } else if (yPos < driveTrain.getPoseEstimate().getY() - 0.03){
             ySpeed = -1.0;
             y = false;
         } else {
@@ -108,10 +121,10 @@ public class testAuto extends CommandBase {
             y = true;
         }
 
-        if (thetaPos > driveTrain.getPoseEstimate().getRotation().getDegrees() + 5.0) {
+        if (thetaPos > driveTrain.getPoseEstimate().getRotation().getDegrees() + 3.0) {
             thetaSpeed = 1.0;
             theta = false;
-        } else if (thetaPos < driveTrain.getPoseEstimate().getRotation().getDegrees() - 5.0){
+        } else if (thetaPos < driveTrain.getPoseEstimate().getRotation().getDegrees() - 3.0){
             thetaSpeed = -1.0;
             theta = false;
         } else {
